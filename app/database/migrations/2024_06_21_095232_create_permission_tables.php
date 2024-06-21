7c737e50e45709d0f511d8de19a20d02d719d1ea
PawikSD <pawel@jjuchim.com>
2024-06-21 12:14:25 +0200

    Migrate

--- app/database/migrations/2024_06_21_095232_create_permission_tables.php
+++ app/database/migrations/2024_06_21_095232_create_permission_tables.php
@@ -4,6 +4,8 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 use Spatie\Permission\PermissionRegistrar;
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\Models\Permission;
 
 class CreatePermissionTables extends Migration
 {
@@ -113,6 +115,12 @@ class CreatePermissionTables extends Migration
 
             $table->primary([PermissionRegistrar::$pivotPermission, PermissionRegistrar::$pivotRole], 'role_has_permissions_permission_id_role_id_primary');
         });
+
+        $roles = ['admin', 'editor', 'client', 'viewer'];
+
+        foreach ($roles as $role) {
+            Role::create(['name' => $role]);
+        }
 
         app('cache')
             ->store(config('permission.cache.store') != 'default' ? config('permission.cache.store') : null)
