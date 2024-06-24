2a08728f08366ae2fabe8aff30cbbe00f5bcb9d1
Pawel Juchim <pawel@juchim.com>
2024-06-24 23:00:20 +0200

    jobs update

--- app/app/Console/Kernel.php
+++ app/app/Console/Kernel.php
@@ -16,6 +16,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         $schedule->job(new FetchSpotifyNewReleases)->daily();
+        $schedule->job(new UpdateMissingContactInfo)->daily()->at('00:00');
     }
 
     /**
