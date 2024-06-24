95c687583bf4f57fd8c7d291cf966b3fc97b5c4c
Pawel Juchim <pawel@juchim.com>
2024-06-24 23:02:14 +0200

    Ibserver added

---
+++ app/app/Observers/ArtistObserver.php
@@ -0,0 +1,17 @@
+<?php
+namespace App\Observers;
+
+use App\Models\Artist;
+use App\Jobs\FetchSpotifyArtistDetails;
+
+class ArtistObserver
+{
+    public function created(Artist $artist)
+    {
+        FetchSpotifyArtistDetails::dispatch($artist)
+            ->delay(now()->addMinutes(3))
+            ->onQueue('default')
+            ->retryAfter(60)
+            ->tries(3);
+    }
+}
--- app/app/Providers/EventServiceProvider.php
+++ app/app/Providers/EventServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\Models\Artist;
+use App\Observers\ArtistObserver;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Auth\Listeners\SendEmailVerificationNotification;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
@@ -29,6 +31,7 @@ class EventServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        parent::boot();
+        Artist::observe(ArtistObserver::class);
     }
 }
