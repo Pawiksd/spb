55de2d2930521ce207e9995e1aed278396b9f87f
Pawel Juchim <pawel@juchim.com>
2024-06-26 23:08:45 +0200

    Job2 update

--- app/app/Jobs/FetchSpotifyNewReleases.php
+++ app/app/Jobs/FetchSpotifyNewReleases.php
@@ -10,6 +10,7 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\Log;
 
 class FetchSpotifyNewReleases implements ShouldQueue
 {
@@ -22,31 +23,49 @@ class FetchSpotifyNewReleases implements ShouldQueue
 
     public function handle()
     {
+        Log::info('Fetching new releases from Spotify.');
+
         $spotifyService = new SpotifyService();
         $newReleases = $spotifyService->getNewReleases();
 
+        Log::info('New releases fetched.', ['count' => count($newReleases['albums']['items'])]);
+
         foreach ($newReleases['albums']['items'] as $album) {
-            $artistData = $album['artists'][0];
-            $artist = Artist::firstOrCreate(
-                ['spotify_id' => $artistData['id']],
-                ['name' => $artistData['name']]
-            );
+            try {
+                Log::info('Processing album.', ['album' => $album['name']]);
 
-            // Sprawdź, czy NewRelease już istnieje
-            $existingRelease = NewRelease::where('spotify_id', $album['id'])->first();
+                $artistData = $album['artists'][0];
+                $artist = Artist::firstOrCreate(
+                    ['spotify_id' => $artistData['id']],
+                    ['name' => $artistData['name']]
+                );
 
-            if (!$existingRelease) {
-                NewRelease::create([
-                    'spotify_id' => $album['id'],
-                    'title' => $album['name'],
-                    'artist_id' => $artist->id,
-                    'release_date' => $album['release_date'],
-                    'genre' => $album['genres'][0] ?? null,
-                    'label' => $album['label'] ?? null,
+                $existingRelease = NewRelease::where('spotify_id', $album['id'])->first();
+
+                if (!$existingRelease) {
+                    NewRelease::create([
+                        'spotify_id' => $album['id'],
+                        'title' => $album['name'],
+                        'artist_id' => $artist->id,
+                        'release_date' => $album['release_date'],
+                        'genre' => $album['genres'][0] ?? null,
+                        'label' => $album['label'] ?? null,
+                    ]);
+
+                    Log::info('New release added.', ['title' => $album['name']]);
+                } else {
+                    Log::info('Release already exists.', ['title' => $album['name']]);
+                }
+            } catch (\Exception $e) {
+                Log::error('Error processing album.', [
+                    'album' => $album['name'],
+                    'error' => $e->getMessage(),
                 ]);
-
-                FetchSpotifyArtistDetails::dispatch($artist);
+                // Continue with the next album
+                continue;
             }
         }
+
+        Log::info('Processing of new releases completed.');
     }
 }
