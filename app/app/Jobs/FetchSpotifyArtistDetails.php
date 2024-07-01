469401b35420c6ae8e57aaa4ee63427ad13ea754
Pawel Juchim <pawel@juchim.com>
2024-07-01 13:03:52 +0200

    Job update

--- app/app/Jobs/FetchSpotifyArtistDetails.php
+++ app/app/Jobs/FetchSpotifyArtistDetails.php
@@ -9,10 +9,14 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\Log;
 
 class FetchSpotifyArtistDetails implements ShouldQueue
 {
     use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    public $retryAfter = 60;
+    public $tries = 3;
 
     protected $artist;
 
@@ -26,14 +30,36 @@ class FetchSpotifyArtistDetails implements ShouldQueue
         $spotifyService = new SpotifyService();
         $artistDetails = $spotifyService->getArtistDetails($this->artist->spotify_id);
 
-        $this->artist->update([
-            'email' => $artistDetails['email'] ?? null,
-            'instagram' => $artistDetails['instagram'] ?? null,
-            'facebook' => $artistDetails['facebook'] ?? null,
-            'website' => $artistDetails['website'] ?? null,
-            'youtube' => $artistDetails['youtube'] ?? null,
-        ]);
+        $updateData = [];
 
-        FetchArtistContactInfoFromWebsite::dispatch($this->artist);
+        if (!empty($artistDetails['email'])) {
+            $updateData['email'] = $artistDetails['email'];
+        }
+
+        if (!empty($artistDetails['instagram'])) {
+            $updateData['instagram'] = $artistDetails['instagram'];
+        }
+
+        if (!empty($artistDetails['facebook'])) {
+            $updateData['facebook'] = $artistDetails['facebook'];
+        }
+
+        if (!empty($artistDetails['website'])) {
+            $updateData['website'] = $artistDetails['website'];
+        }
+
+        if (!empty($artistDetails['youtube'])) {
+            $updateData['youtube'] = $artistDetails['youtube'];
+        }
+
+        if (!empty($artistDetails['twitter'])) {
+            $updateData['twitter'] = $artistDetails['twitter'];
+        }
+
+        if (!empty($updateData)) {
+            $this->artist->update($updateData);
+        }
+
+        FetchArtistContactInfoFromWebsite::dispatch($this->artist); //->onQueue('fetch-artist-info');
     }
 }
