99bac06ac8ba1db447606222da7fe63db2f2ef66
Pawel Juchim <pawel@juchim.com>
2024-07-01 13:03:23 +0200

    Kernel update

--- app/app/Console/Kernel.php
+++ app/app/Console/Kernel.php
@@ -7,27 +7,15 @@ use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
 class Kernel extends ConsoleKernel
 {
-    /**
-     * Define the application's command schedule.
-     *
-     * @param  \Illuminate\Console\Scheduling\Schedule  $schedule
-     * @return void
-     */
     protected function schedule(Schedule $schedule)
     {
         $schedule->job(new FetchSpotifyNewReleases)->daily();
         $schedule->job(new UpdateMissingContactInfo)->daily()->at('00:00');
     }
 
-    /**
-     * Register the commands for the application.
-     *
-     * @return void
-     */
     protected function commands()
     {
         $this->load(__DIR__.'/Commands');
-
         require base_path('routes/console.php');
     }
 }
