447fca9f3df2cd6e1c7fcc25f9e91ca50f6eafda
Pawel Juchim <pawel@juchim.com>
2024-07-02 14:12:33 +0200

    Profile style updated

--- app/resources/views/profile/show.blade.php
+++ app/resources/views/profile/show.blade.php
@@ -1,6 +1,6 @@
 <x-app-layout>
     <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight header">
             {{ __('Profile') }}
         </h2>
     </x-slot>
@@ -9,9 +9,25 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
-                    <p>Name: {{ $user->name }}</p>
-                    <p>Email: {{ $user->email }}</p>
-                    <p>Roles: {{ implode(', ', $user->getRoleNames()->toArray()) }}</p>
+                    <h3 class="text-lg font-medium text-gray-900">User Profile</h3>
+                    <div class="table-container mt-4">
+                        <table class="min-w-full divide-y divide-gray-200">
+                            <tbody class="bg-white divide-y divide-gray-200">
+                                <tr>
+                                    <td class="px-6 py-4 whitespace-nowrap font-semibold">Name:</td>
+                                    <td class="px-6 py-4 whitespace-nowrap">{{ $user->name }}</td>
+                                </tr>
+                                <tr>
+                                    <td class="px-6 py-4 whitespace-nowrap font-semibold">Email:</td>
+                                    <td class="px-6 py-4 whitespace-nowrap">{{ $user->email }}</td>
+                                </tr>
+                                <tr>
+                                    <td class="px-6 py-4 whitespace-nowrap font-semibold">Roles:</td>
+                                    <td class="px-6 py-4 whitespace-nowrap">{{ implode(', ', $user->getRoleNames()->toArray()) }}</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
                 </div>
             </div>
         </div>
