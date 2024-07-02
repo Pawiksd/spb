1108c964599d049dc6e634b0b647d104883629f8
Pawel Juchim <pawel@juchim.com>
2024-07-02 14:12:02 +0200

    New releases job updated

--- app/app/Jobs/FetchSpotifyNewReleases.php
+++ app/app/Jobs/FetchSpotifyNewReleases.php
@@ -30,6 +30,127 @@ class FetchSpotifyNewReleases implements ShouldQueue
 
         Log::info('New releases fetched.', ['count' => count($newReleases['albums']['items'])]);
 
+
+        foreach ($newReleases['albums']['items'] as $album) {
+            try {
+                Log::info('Processing album.', ['album' => $album['name']]);
+
+                $artistData = $album['artists'][0];
+                $artist = Artist::firstOrCreate(
+                    ['spotify_id' => $artistData['id']],
+                    ['name' => $artistData['name']]
+                );
+
+                $existingRelease = NewRelease::where('spotify_id', $album['id'])->first();
+
+                if (!$existingRelease) {
+                    NewRelease::create([
+                        'spotify_id' => $album['id'],
+                        'title' => $album['name'],
+                        'artist_id' => $artist->id,
+                        'release_date' => $album['release_date'],
+                        'genre' => $album['genres'][0] ?? null,
+                        'label' => $album['label'] ?? null,
+                    ]);
+
+                    Log::info('New release added.', ['title' => $album['name']]);
+                } else {
+                    Log::info('Release already exists.', ['title' => $album['name']]);
+                }
+            } catch (\Exception $e) {
+                Log::error('Error processing album.', [
+                    'album' => $album['name'],
+                    'error' => $e->getMessage(),
+                ]);
+                continue;
+            }
+        }
+
+        $newReleases = $spotifyService->getNewReleases(51);
+
+        Log::info('New releases fetched.', ['count' => count($newReleases['albums']['items'])]);
+
+
+        foreach ($newReleases['albums']['items'] as $album) {
+            try {
+                Log::info('Processing album.', ['album' => $album['name']]);
+
+                $artistData = $album['artists'][0];
+                $artist = Artist::firstOrCreate(
+                    ['spotify_id' => $artistData['id']],
+                    ['name' => $artistData['name']]
+                );
+
+                $existingRelease = NewRelease::where('spotify_id', $album['id'])->first();
+
+                if (!$existingRelease) {
+                    NewRelease::create([
+                        'spotify_id' => $album['id'],
+                        'title' => $album['name'],
+                        'artist_id' => $artist->id,
+                        'release_date' => $album['release_date'],
+                        'genre' => $album['genres'][0] ?? null,
+                        'label' => $album['label'] ?? null,
+                    ]);
+
+                    Log::info('New release added.', ['title' => $album['name']]);
+                } else {
+                    Log::info('Release already exists.', ['title' => $album['name']]);
+                }
+            } catch (\Exception $e) {
+                Log::error('Error processing album.', [
+                    'album' => $album['name'],
+                    'error' => $e->getMessage(),
+                ]);
+                continue;
+            }
+        }
+
+        $newReleases = $spotifyService->getNewReleases(101);
+
+        Log::info('New releases fetched.', ['count' => count($newReleases['albums']['items'])]);
+
+
+        foreach ($newReleases['albums']['items'] as $album) {
+            try {
+                Log::info('Processing album.', ['album' => $album['name']]);
+
+                $artistData = $album['artists'][0];
+                $artist = Artist::firstOrCreate(
+                    ['spotify_id' => $artistData['id']],
+                    ['name' => $artistData['name']]
+                );
+
+                $existingRelease = NewRelease::where('spotify_id', $album['id'])->first();
+
+                if (!$existingRelease) {
+                    NewRelease::create([
+                        'spotify_id' => $album['id'],
+                        'title' => $album['name'],
+                        'artist_id' => $artist->id,
+                        'release_date' => $album['release_date'],
+                        'genre' => $album['genres'][0] ?? null,
+                        'label' => $album['label'] ?? null,
+                    ]);
+
+                    Log::info('New release added.', ['title' => $album['name']]);
+                } else {
+                    Log::info('Release already exists.', ['title' => $album['name']]);
+                }
+            } catch (\Exception $e) {
+                Log::error('Error processing album.', [
+                    'album' => $album['name'],
+                    'error' => $e->getMessage(),
+                ]);
+                continue;
+            }
+        }
+
+        $newReleases = $spotifyService->getNewReleases(151);
+
+        Log::info('New releases fetched.', ['count' => count($newReleases['albums']['items'])]);
+
+
         foreach ($newReleases['albums']['items'] as $album) {
             try {
                 Log::info('Processing album.', ['album' => $album['name']]);
