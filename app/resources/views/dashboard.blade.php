92768ec64b662497ec303963292c5a62f281c1ab
Pawel Juchim <pawel@juchim.com>
2024-07-02 14:12:30 +0200

    Dashboard style updated

--- app/resources/views/dashboard.blade.php
+++ app/resources/views/dashboard.blade.php
@@ -9,7 +9,7 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
-                    <h3 class="text-lg font-medium text-gray-900">Latest 50 New Releases</h3>
+                    <h3 class="text-lg font-medium text-gray-900 header">Latest 50 New Releases</h3>
                     @if ($latestReleases->isNotEmpty())
                         <div class="table-container mt-4">
                             <table class="min-w-full divide-y divide-gray-200">
@@ -52,4 +52,5 @@
         <div class="popup-body"></div>
         <div class="popup-close">&times;</div>
     </div>
+
 </x-app-layout>
